8579725f8561e3a50bafbf917ecb3604e45423df
motah-fard <motah.fard@gmail.com>
2021-11-04 15:29:40 -0700

    hangman letter part

--- client/src/components/Hangman.js
+++ client/src/components/Hangman.js
@@ -1,21 +1,32 @@
-import React, {Component, useState} from 'react';
-import './Hangman.css'
-import RandomWord from './RandomWord'
-import ReactDOMServer from 'react-dom/server'
-
-
+import React, { Component, useState } from "react";
+import "./Hangman.css";
+import RandomWord from "./RandomWord";
+import ReactDOMServer from "react-dom/server";
 
 const Hangman = (props) => {
-    let a = ReactDOMServer.renderToString(RandomWord())
-    a = a.replace('<div data-reactroot="">',"")
-    let randomWord = a.replace('</div>','')
-    const [word,setWord] = useState('')
-    console.log(randomWord );
-    return (
-        <div>
+  let a = ReactDOMServer.renderToString(RandomWord());
+  a = a.replace('<div data-reactroot="">', "");
+  const randomWord = a.replace("</div>", "").toUpperCase();
+  var output = randomWord.split("");
+  console.log(randomWord.length);
+  console.log(output);
+  let b = "-";
+//   let testarray = [];
+//   for (let i = 0; i < randomWord.length; i++) {
+//       testarray.push(b)
+      
+//   }
+  let letters = ['A'];
+  const guess = output.map((letter) => letters.includes(letter) ? letter : '-')
+  console.log(guess)
+  // randomWord.length
 
-        </div>
-    );
+
+  return (
+    <div>
+      {guess}
+    </div>
+  );
 };
 
 export default Hangman;
