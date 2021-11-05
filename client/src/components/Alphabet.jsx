f2617e8eee7be9153253b6dbbf15e1ad32eff9a7
motah-fard <motah.fard@gmail.com>
2021-11-04 20:56:00 -0700

    buttons

--- client/src/components/Alphabet.jsx
+++ client/src/components/Alphabet.jsx
@@ -1,13 +1,22 @@
-import React, { Component, useState } from "react";
-
+import React from "react";
+import { useEffect, useState } from "react";
 
 function Alphabet (){
-  const [buttonLetter, setButtonLetter] = useState ('');
+  const [buttonLetter, setButtonLetter] = useState('')
+  const [toggle, setToggle] = useState(false);
+  const handleClick = (e) => {
+    e.preventDefault();
+    const array = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
+    array.map((letter)=>{
+      setButtonLetter(letter)
+      console.log(letter);
+    },[toggle])
+  }
   function getButtons () {
     const array = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
-
+ 
     return array.map((letter) => {
-      return <button>{letter}</button>;
+      return <button onClick={handleClick}>{letter}</button>;
     });
   };
 
